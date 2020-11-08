fb69f8f96c2da6907812f65e1f02a50f58480229
M4ster3001 <aldocfjunior@gmail.com>
2020-11-08 12:00:59 -0300

    aplicado as alterações para o typeorm Repository

--- src/database/migrations/1604837053507-CreateAppointments.ts
+++ src/database/migrations/1604837053507-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1604837053507
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,26 +1,26 @@
 import { Request, Response, Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request: Request, response: Response) => {
-    return response.json(appointmentsRepository.listAllAppointments());
+appointmentsRouter.get('/', async (request: Request, response: Response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    return response.json(await appointmentsRepository.find());
 });
 
-appointmentsRouter.post('/', (request: Request, response: Response) => {
+appointmentsRouter.post('/', async (request: Request, response: Response) => {
     try {
         const { provider, date } = request.body;
 
         const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService(
-            appointmentsRepository,
-        );
+        const createAppointment = new CreateAppointmentService();
 
-        const appointment = createAppointment.execute({
+        const appointment = await createAppointment.execute({
             date: parsedDate,
             provider,
         });
