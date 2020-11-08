fe89a84cae54da2d95ea7fefde6e67a1f95871fd
M4ster3001 <aldocfjunior@gmail.com>
2020-11-08 16:09:06 -0300

    criado a função de criar usuarios e alterado a criação dos appointments

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from 'express';
+import CreateUserService from '../services/CreateUsersService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request: Request, response: Response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password,
+        });
+
+        return response.json(user);
+    } catch (err) {
+        return response.status(400).json({ erro: err.message });
+    }
+});
+
+export default usersRouter;
