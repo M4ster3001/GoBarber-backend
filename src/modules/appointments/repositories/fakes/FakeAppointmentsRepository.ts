80e80d914ff7c4ac871a427ac7c2998b1b9a3e30
M4ster3001 <aldocfjunior@gmail.com>
2021-01-31 18:33:03 -0300

    terminado os testes de appointments

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -2,14 +2,16 @@ import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointment
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import { uuid } from 'uuidv4';
+import { v4 } from 'uuid';
+import { isEqual } from 'date-fns';
 
 // Repository seria como se fosse uma função e o <Appointment> seria um parametro
 class FakeAppointmentsRepository implements IAppointmentsRepository {
     private appointments: Appointment[] = [];
 
     public async findByDate(date: Date): Promise<Appointment | undefined> {
-        const findAppointment = this.appointments.find(appointment => appointment.date === date);
+        const findAppointment = this.appointments.find(appointment =>
+            isEqual(appointment.date, date));
 
         return findAppointment;
     }
@@ -17,7 +19,7 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
     public async create({provider_id, date}: ICreateAppointmentDTO): Promise<Appointment>{
         const appointment = new Appointment();
 
-        Object.assign(appointment, {id: uuid(), date, provider_id})
+        Object.assign(appointment, {id: v4(), date, provider_id})
 
         this.appointments.push(appointment);
 
